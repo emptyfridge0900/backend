5ff752c1f0b62da0a3e08b5bd0e63140c3af68e5
Doosan Beak <emptyfridge0900@gmail.com>
2025-04-04 00:32:51 -0400

    run without port number

--- Book.Server/Program.cs
+++ Book.Server/Program.cs
@@ -46,9 +46,8 @@ namespace BookStore.Server
             {
                 spa.Options.SourcePath = "book.client";
             });
-            //app.Run();
-            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-            app.Run($"http://0.0.0.0:{port}");
+            app.Run();
+
         }
     }
 }
